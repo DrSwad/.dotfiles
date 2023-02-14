dc4c0c63f6a826915ec5cbec868da7653bf94629
Nayeemul Islam Swad <drswad2013@gmail.com>
2023-02-14 19:59:40 +0600

    Make yt-mp3 downloaded thumbnails square

--- oh-my-zsh/custom/yt-mp3.zsh
+++ oh-my-zsh/custom/yt-mp3.zsh
@@ -1,3 +1,3 @@
 yt-mp3() {
-  youtube-dl --extract-audio --embed-thumbnail --audio-format mp3 -o '~/Downloads/Tmp Musics/%(title)s.%(ext)s' $@
+  youtube-dl --extract-audio --embed-thumbnail --audio-format mp3 --postprocessor-args "-c:v mjpeg -vf crop=\"'if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\"" -o '~/Downloads/Tmp Musics/%(title)s.%(ext)s' $@
 }
